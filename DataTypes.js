b933444578be23523a571b525037db0bf10bc60c
Saad123ali <itmesaad@gmail.com>
2024-09-23 14:48:32 +0500

    Add Comment

--- DataTypes.js
+++ DataTypes.js
@@ -7,11 +7,11 @@ let str = "Hello, World!";
 let bool = false;
 let undef;
 let nul = null;
-console.log(num, typeof num);    
-console.log(str, typeof str);    
-console.log(bool, typeof bool);  
-console.log(undef, typeof undef); 
-console.log(nul, typeof nul);
+console.log(num, typeof num);             //[42 , 'number]
+console.log(str, typeof str);             //['Hello world!' , 'string]
+console.log(bool, typeof bool);           //[false , 'boolean]
+console.log(undef, typeof undef);         //[undefined , 'undefined']
+console.log(nul, typeof nul);             //[null . 'object]
 
 
                                             // Non - Primitive Data Types
@@ -24,12 +24,12 @@ let person = {
     isEmployed: true
 };
 
-console.log(person, typeof person); 
+console.log(person, typeof person);     //[  { name: "Alice", age: 30, isEmployed: true } , 'object]
                
 // Array Types but it type is object
 
 let numbers = [1, 2, 3, 4, 5];
-console.log(numbers, typeof numbers); 
+console.log(numbers, typeof numbers);   //[ [1, 2, 3, 4, 5] , 'object']
 
 
 // Function Types
@@ -38,5 +38,5 @@ function greet(name) {
     return "Hello, " + name + "!";
 }
 
-console.log(greet, typeof greet); 
-console.log(greet("Alice"));      
+console.log(greet, typeof greet);       //['function'] 
+console.log(greet("Alice"));            //'Hello , Alice!'
