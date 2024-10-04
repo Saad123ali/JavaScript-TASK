ee211b9121cf8f5632b79bc40b6134da8f22d645
Saad123ali <itmesaad@gmail.com>
2024-10-04 21:32:36 +0500

    Conditional-Expression

--- Conditional-Expression.js
+++ Conditional-Expression.js
@@ -1,37 +1,37 @@
 // Conditional Expression in JavaScript
 
-// Sometimes we might have to execute a block of code based on some condition 
+// Conditional expressions allow us to execute different blocks of code based on certain conditions. 
 
 // Example: Enter age 
 
-// There are three types of conditional expressions 
+// There are three types of conditional expressions: 
 
-//   1.   if statement
-//   2.   if ... else statement
-//   3.   if ... else ... if statement
-
-// Hardcoded user input for age (you can change this value)
-let age = 25; // Example age
+//   1.   if statement: 
+//        The if statement executes a block of code if a specified condition is true.
+let age = 25; // Hardcoded user input for age (you can change this value)
 
 // 1. if statement
 if (age >= 18) {
-    console.log("You are an adult.");
+    console.log("You are an adult."); // This will be executed if the condition is true
 }
 
-// 2. if ... else statement
+//   2.   if ... else statement: 
+//        The if ... else statement executes one block of code if the condition is true and another block if it is false.
 if (age < 18) {
-    console.log("You are a minor.");
+    console.log("You are a minor."); // This will be executed if the condition is true
 } else {
-    console.log("You are an adult.");
+    console.log("You are an adult."); // This will be executed if the condition is false
 }
 
-// 3. if ... else ... if statement
+//   3.   if ... else ... if statement: 
+//        The if ... else ... if statement allows you to specify multiple conditions to test. 
+//        If the first condition is false, it checks the next condition, and so on.
 if (age < 13) {
-    console.log("You are a child.");
+    console.log("You are a child."); // Executed if age is less than 13
 } else if (age >= 13 && age < 18) {
-    console.log("You are a teenager.");
+    console.log("You are a teenager."); // Executed if age is between 13 and 17
 } else if (age >= 18 && age < 65) {
-    console.log("You are an adult.");
+    console.log("You are an adult."); // Executed if age is between 18 and 64
 } else {
-    console.log("You are a senior.");
+    console.log("You are a senior."); // Executed if age is 65 or older
 }
